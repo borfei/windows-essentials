539c95c572248829f8a71bea197c440c0af12579
feivegian <feivegian@gmail.com>
2024-12-24 12:37:16 +0800

    refactor: Add copyright to each script/registry file

--- scripts/DisablePrefetchnPrelaunch.ps1
+++ scripts/DisablePrefetchnPrelaunch.ps1
@@ -1,2 +1,6 @@
+# Author: リェンーゆく (teamworks1732@gmail.com)
+#
+# This file is part of Windows Essentials, a repository dedicated to enhance Windows experience.
+# Windows Essentials is licensed under The Unlicense license, feel free to modify anything.
 Disable-MMAgent -ApplicationPreLaunch
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters" /v EnablePrefetcher /t REG_DWORD /d "0" /f
--- scripts/SSDTuning.bat
+++ scripts/SSDTuning.bat
@@ -1,3 +1,7 @@
+:: Author: リェンーゆく (teamworks1732@gmail.com)
+::
+:: This file is part of Windows Essentials, a repository dedicated to enhance Windows experience.
+:: Windows Essentials is licensed under The Unlicense license, feel free to modify anything.
 @echo off
 fsutil behavior set DisableLastAccess 1
 fsutil behavior set EncryptPagingFile 0
