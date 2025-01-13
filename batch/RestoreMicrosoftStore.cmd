ee9a5ab44e28c151a99ce3ae4f180da2f0f9ae9e
feivegian <feivegian@gmail.com>
2025-01-13 15:52:51 +0800

    feat: Add Microsoft Store restoration

---
+++ batch/RestoreMicrosoftStore.cmd
@@ -0,0 +1,6 @@
+:: Author: Borfei (feivegian@gmail.com)
+::
+:: This file is part of Windows Essentials, a repository dedicated to enhance Windows experience.
+:: Windows Essentials is licensed under The Unlicense license, feel free to modify anything.
+@echo off
+wsreset -i
